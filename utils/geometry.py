77db844187a42d5c117525a3f82fa05edf1a8ba4
esoberanis <emmanualsob@gmail.com>
2025-12-03 12:49:01 -0600

    Modificaciones a algunas funciones

--- utils/geometry.py
+++ utils/geometry.py
@@ -6,24 +6,24 @@ def cosd(x):return np.cos(np.deg2rad(x))
 def asind(x):return np.degrees(np.arcsin(np.clip(x, -1.0, 1.0)))
 def acosd(x):return np.degrees(np.arccos(np.clip(x, -1.0, 1.0)))
 #-------------------------------------------------------------------------------
-def declinacion_solar(n): 
-    return 23.45 * sind(360.0 * (n-80) / 365.0)
+def declinacion_solar(d): 
+    return 23.45 * sind(360.0 * (d-80) / 365.0)
 #-------------------------------------------------------------------------------
 def angulo_w(T): 
     return (15*(12-T))
 #-------------------------------------------------------------------------------
-def altura_solar(n,phi,T):
-    return asind(sind(declinacion_solar(n))*sind(phi) + cosd(declinacion_solar(n))*cosd(phi)*cosd(angulo_w(T)))
+def altura_solar(d,T,phi):
+    return asind(sind(declinacion_solar(d))*sind(phi) + cosd(declinacion_solar(d))*cosd(phi)*cosd(angulo_w(T)))
 #-------------------------------------------------------------------------------
-def angulo_zenith(n,phi,T):
-    return 90 - altura_solar(n,phi,T)
+def angulo_zenith(d,T,phi):
+    return 90 - altura_solar(d,T,phi)
 #-------------------------------------------------------------------------------
-def angulo_azimut(n,phi,T):
+def angulo_azimut(d,T,phi):
     return acosd(
-        (sind(altura_solar(n,phi,T))*sind(phi)-sind(declinacion_solar(n))) / (cosd(altura_solar(n,phi,T))*cosd(phi))
+        (sind(altura_solar(d,T,phi))*sind(phi)-sind(declinacion_solar(d))) / (cosd(altura_solar(d,T,phi))*cosd(phi))
     )
 #-------------------------------------------------------------------------------
-def angulo_incidente(n,phi,T,b):
+def angulo_incidente(d,T,phi,b):
     return acosd(
-        sind(angulo_zenith(n,phi,T))*sind(angulo_azimut(n,phi,T)) + sind(angulo_zenith(n,phi,T))*cosd(angulo_azimut(n,phi,T))*sind(b) + cosd(angulo_zenith(n,phi,T))*cosd(b)
+        sind(angulo_zenith(d,T,phi))*sind(angulo_azimut(d,T,phi)) + sind(angulo_zenith(d,T,phi))*cosd(angulo_azimut(d,T,phi))*sind(b) + cosd(angulo_zenith(d,T,phi))*cosd(b)
     )
